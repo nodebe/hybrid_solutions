b533696741499db7a92ab0d2df9bbdc6041f72bd
nodebe <nodywelete3@gmail.com>
2022-10-27 20:06:21 +0100

    set rearrange config to false

--- task/__init__.py
+++ task/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .users.routes import user_route
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 # Registering blueprint
 app.register_blueprint(user_route)
