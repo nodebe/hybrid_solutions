4de459e6a9abd3811dc9219ae0f89d68026d9145
nodebe <nodywelete3@gmail.com>
2022-10-28 14:23:46 +0100

    added cors

--- task/__init__.py
+++ task/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from .users.routes import user_route
 
 app = Flask(__name__)
+CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 # Registering blueprint
